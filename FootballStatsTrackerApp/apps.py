24aea0c6efa0a6b8405fba802459b80154d4b922
rlusio <radoslawlusio@gmail.com>
2024-12-05 12:55:48 +0100

    Add files via upload

---
+++ FootballStatsTrackerApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FootballstatstrackerappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'FootballStatsTrackerApp'
