b4a6a1921834565536c4f9c05235c6a26efb2e00
Grzegorz Adamek <grzegorz0852@onet.pl>
2024-12-12 22:34:52 +0100

    adding models for teams and matches

--- all_players/models.py
+++ all_players/models.py
@@ -9,3 +9,19 @@ class Player(models.Model):
     shirtNumber=models.IntegerField(null=True)
     marketValue=models.IntegerField(null=True)
     # Create your models here.
+class Team(models.Model):
+    Team_Name = models.CharField(max_length=255,null=True)
+    Country = models.CharField(max_length=255,null=True)
+    City =models.CharField(max_length=255 ,null=True)
+    marketValue=models.IntegerField(null=True)
+    Number_of_Players = models.IntegerField(null=True)
+    Standings= models.IntegerField(null=True)
+
+class Match(models.Model):
+    Competators = models.CharField(max_length=255,null=True)
+    Match_Place = models.CharField(max_length=255,null=True)
+    Competetion = models.CharField(max_length=255 , null=True)
+    Season_Start_date =models.DateField(null=True)
+    Season_End_date =models.DateField(null=True)
+    Score = models.CharField(max_length=255 ,null=True)
+    Status = models.CharField(max_length=255, null=True)
