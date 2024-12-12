a7906727d9508c6ba7ca91f7a08013126cff7ed3
Grzegorz Adamek <grzegorz0852@onet.pl>
2024-12-12 22:38:25 +0100

    adding views for matches and teams

--- all_players/views.py
+++ all_players/views.py
@@ -1,7 +1,11 @@
 from .models import Player
 from django.http import HttpResponse
 from django.template import loader
+from .models import Player
+from .models import Team
+from .models import Match
 from django.shortcuts import render
+
 
 def all_players(request):
     myplayers = Player.objects.all()
@@ -14,6 +18,36 @@ def details(request,id):
         'myplayer': myplayer,
          }
     return HttpResponse(template.render(context,request))
+
+def all_teams(request):
+    myteams =Team.objects.all().values()
+    template = loader.get_template('all_teams/all_teams.html')
+    context={
+        'myteams': myteams,
+        }
+    return HttpResponse(template.render(context, request))
+def team_details(request,id):
+    myteam =Team.objects.get(id=id)
+    template = loader.get_template('all_teams/team_details.html')
+    context={
+        'myteam': myteam,
+        }
+    return HttpResponse(template.render(context, request))
+
+def all_matches(request):
+    mymatches =Match.objects.all().values()
+    template = loader.get_template('all_matches/all_matches.html')
+    context={
+        'mymatches': mymatches,
+        }
+    return HttpResponse(template.render(context, request))
+def match_details(request,id):
+    mymatch = Match.objects.all().get(id=id)
+    template = loader.get_template('all_matches/match_details.html')
+    context={
+        'mymatch': mymatch,
+        }
+    return HttpResponse(template.render(context, request))
 
 def main(request):
     return render(request, 'main/home.html')
