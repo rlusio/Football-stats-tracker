eb0a83733af9ca4b28a4c871faa4bb5da0acf185
Grzegorz Adamek <grzegorz0852@onet.pl>
2024-12-13 00:29:58 +0100

    adding urls for the details pages

--- all_players/urls.py
+++ all_players/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
 
     path('details/<int:id>/', views.details,name='details'),
     path('all_matches/', views.all_matches, name='all_matches'),
+    path('all_matches/match_details/<int:id>/', views.match_details, name='match_details'),
     path('all_teams/', views.all_teams, name='all_teams'),
+    path('all_teams/team_details/<int:id>/', views.team_details, name='team_details'),
+
     
 ]
