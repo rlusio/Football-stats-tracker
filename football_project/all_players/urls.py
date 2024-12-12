fcc5ec17ebcba741a508db9ff393723c131251e2
rlusio <radoslawlusio@gmail.com>
2024-12-12 18:46:56 +0100

    Delete football_project directory

--- football_project/all_players/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name='main'),
-    path('all_players/', views.all_players, name='all_players'),
-    path('all_players/details/<int:id>', views.details,name='details'),
-    
-]
