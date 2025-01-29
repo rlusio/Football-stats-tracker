df30c659843a5d09ba1138f5edf983ab6b5fb2a5
Radek <radoslawlusio@gmail.com>
2025-01-29 22:04:24 +0100

    render issues again

--- down_from_google_drive.py
+++ down_from_google_drive.py
@@ -2,7 +2,10 @@ import pandas as pd
 import requests
 from sqlalchemy import create_engine
 import os
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 def download_files_from_google_drive():
     files = {
         "seasons.csv": "1t79cwhBgnfrze0Cn63SDlllGaKnv93p8",
@@ -12,21 +15,24 @@ def download_files_from_google_drive():
         "competition_teams.json": "158d7G5AWEPZ19vuto1WiKZPKM5n8n77h",
         "competition_standings.json": "1FVEX35PbUdVR3YkvXDyrasfuA3NpNA7O",
     }
-    os.makedirs("downloaded_files", exist_ok=True)
+    os.makedirs("data", exist_ok=True)
     for filename, file_id in files.items():
         url = f"https://drive.google.com/uc?export=download&id={file_id}"
         response = requests.get(url, stream=True)
-        file_path = os.path.join("downloaded_files", filename)
+        file_path = os.path.join("data", filename)
         with open(file_path, "wb") as f:
             f.write(response.content)
+        logger.info(f"File {filename} has been downoladed and saved in {file_path}")
         
-def load_csv_to_database(engine, folder="downloaded_files"):
+def load_csv_to_database(engine, folder="data"):
     for filename in os.listdir(folder):
         if filename.endswith(".csv"):
             file_path = os.path.join(folder, filename)
             df = pd.read_csv(file_path)
             table_name = filename.split(".")[0]
             df.to_sql(table_name, engine, if_exists="replace", index=False)
+            logger.info(f"File {filename} has been loaded to database as a {table_name}")
+
         elif filename.endswith(".json"):
             file_path = os.path.join(folder, filename)
             with open(file_path, 'r') as file:
@@ -34,6 +40,8 @@ def load_csv_to_database(engine, folder="downloaded_files"):
             table_name = filename.split(".")[0]
             df = pd.DataFrame([data])
             df.to_sql(table_name, engine, if_exists="replace", index=False)
+            logger.info(f"File {filename} has been loaded to database as a {table_name}")
+
 def main():
     engine = create_engine("postgresql://football_stats_tracker_user:kF1QusNOQ9MKB5VdhMWLldIPs21oRhcV@dpg-cu98fmi3esus73b1lec0-a.oregon-postgres.render.com/football_stats_tracker")
     download_files_from_google_drive() 
