4c5d8645dbe5e7d779c18e0156e578b12b41187b
Natalia-Madry <natalia.madry.mn@gmail.com>
2025-01-30 16:40:23 +0100

    Update settings.py

--- FootballStatsTrackerProject/settings.py
+++ FootballStatsTrackerProject/settings.py
@@ -115,9 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "mystaticfiles"
+    os.path.join(BASE_DIR, "mystaticfiles"),
 ]
 
 # Default primary key field type
