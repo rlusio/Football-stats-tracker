47d20871e3c2c4ebcd93a976c2d68ad12633c333
Natalia-Madry <natalia.madry.mn@gmail.com>
2025-01-30 19:03:51 +0100

    Update urls.py

--- FootballStatsTrackerProject/urls.py
+++ FootballStatsTrackerProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +26,7 @@ urlpatterns = [
     path('', include('main.urls')),        
     path('all_players/', include('all_players.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
 
